91573904b7739cabed0b4b73113cab8052769afd
Marcus Trummer <marcus.trummer@valtech.com>
2022-06-23 20:53:10 -0300

    updating wishlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SetupComponent } from './setup/setup.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { DesktopComponent } from './desktop/desktop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SetupComponent,
+    FooterComponent,
+    MenuComponent,
+    AboutComponent,
+    DesktopComponent
   ],
   imports: [
     BrowserModule,
