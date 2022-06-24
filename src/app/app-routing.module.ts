d63c4890845593aa7dc2feb1bc388b207cf415b4
Marcus Trummer <marcus.trummer@valtech.com>
2022-06-23 21:09:20 -0300

    creating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DesktopComponent } from './desktop/desktop.component';
 import { SetupComponent } from './setup/setup.component';
 
 const routes: Routes = [
 
-  {path:'', redirectTo: 'setup', pathMatch:  'full'},
-  {path: 'setup', component: SetupComponent}
+  {path:'', redirectTo: 'dev', pathMatch:  'full'},
+  {path: 'dev', component: SetupComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'setup', component: DesktopComponent}
   
 
 ];
