e81c1784eb4b0c1d57d10a2952e905efe04225e2
Marcus Trummer <marcus.trummer@valtech.com>
2022-07-01 18:03:55 -0300

    adding transparency to modal

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  copyUrl() {
+    window.location.pathname;
+    var dummy = document.createElement('input'),
+    text = window.location.href;
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
   }
-
 }
