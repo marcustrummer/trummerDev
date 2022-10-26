4f63f345ef974ff4469bf2f85269d9394b99c91a
Marcus Trummer <marcus.trummer@valtech.com>
2022-10-26 18:07:37 -0300

    This commit should create component reminder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { DesktopComponent } from './desktop/desktop.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ReminderComponent } from './reminder/reminder.component';
 import { SetupComponent } from './setup/setup.component';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: 'dev', component: SetupComponent},
   {path: 'about', component: AboutComponent},
   {path: 'setup', component: DesktopComponent},
-  {path: 'projects', component:   ProjectsComponent}
+  {path: 'projects', component:   ProjectsComponent},
+  {path: 'reminder', component:   ReminderComponent}
 
   
 
