0f30e7419d141b51263f0fe018972cb3783f188f
Marcus Trummer <marcus.trummer@valtech.com>
2022-11-25 17:08:58 -0300

    this commit should create pronunciationButton && fix homepage

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChildren, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  @ViewChildren('audio') audioElms!: ElementRef;
+  src: string = '../../../assets/namePronunciation.ogg';
+  isPaused = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
+  onPlayClick(audio: HTMLAudioElement) {
+
+    if (!this.isPaused) {
+     audio.play();
+     this.isPaused = true;
+    } else {
+     audio.pause();
+     this.isPaused = false;
+    }
+   }
+   
+    onPause() {
+     this.isPaused = false;
+    }
 
 }
